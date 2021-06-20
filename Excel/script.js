7f2b274a2eebc180ccaa7c981220941703b10022
arvind8998 <asangwan8998@gmail.com>
2021-06-20 21:31:07 +0530

    Added excel features

--- Excel/script.js
+++ Excel/script.js
@@ -1,2 +1,17 @@
-const body = document.querySelector("body");
-body.spellcheck = false;
+let body = document.querySelector("body")
+body.spellcheck = false
+
+let menuBarTags = document.querySelectorAll(".menu-bar p")
+
+for (let i = 0; i < menuBarTags.length; i++) {
+  menuBarTags[i].addEventListener("click", () => {
+    if (menuBarTags[i].classList.contains("menu-bar-option-selected")) {
+      menuBarTags[i].classList.remove("menu-bar-option-selected")
+    } else {
+      for (let j = 0; j < menuBarTags.length; j++) {
+        menuBarTags[j].classList.remove("menu-bar-option-selected")
+      }
+      menuBarTags[i].classList.add("menu-bar-option-selected")
+    }
+  })
+}
