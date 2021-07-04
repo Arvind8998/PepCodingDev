c7274b6ad00165e4b90b80e5fd2763d75ad28d51
arvind8998 <asangwan8998@gmail.com>
2021-07-04 23:03:22 +0530

    Added destructuring and react es6

--- ES6/Spread.js
+++ ES6/Spread.js
@@ -15,8 +15,22 @@
 // console.log(obj1)
 // console.log(obj3)
 
-let data = [1, 2, 3]
+// let data = [1, 2, 3]
 
-let [a,, c] = data
-// let [a, c] = data
-console.log(a, c)
+// let [a,, c] = data
+// // let [a, c] = data
+// console.log(a, c)
+
+let a = {
+  x: {
+    z: {
+        z1:undefined,
+    }
+  },
+  y: "beta",
+}
+
+let {x : {z : {z1:alias ="default"}}} = a
+
+console.log(a)
+console.log(alias)
