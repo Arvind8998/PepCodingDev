fb09901a1754da880eff1dd4cb796c4e74ce6dd5
arvind8998 <asangwan8998@gmail.com>
2021-07-04 19:53:17 +0530

    Added excel scroll feature and spread es6

---
+++ ES6/Spread.js
@@ -0,0 +1,22 @@
+// let obj1 = {
+//     key1 : {
+//         key2: {
+//             key3 :'data'
+//         }
+//     }
+// }
+
+// let obj2 = {key4: 1}
+
+// let obj3 = {...obj1, ...obj2}
+
+// obj1.key1.key2.key3 = 'updated'
+
+// console.log(obj1)
+// console.log(obj3)
+
+let data = [1, 2, 3]
+
+let [a,, c] = data
+// let [a, c] = data
+console.log(a, c)
---
+++ Excel/ScrollScript.js
@@ -0,0 +1,11 @@
+let previousScrollTop = null, previousScrollLeft = null
+
+window.addEventListener("scroll", function(e){
+    let currTopScroll = e.currentTarget.scrollTop
+    if(previousScrollLeft != e.currentTarget.scrollLeft){
+    
+    }
+    else if(previousScrollTop != e.currentTarget.scrollTop){
+    
+    }
+    })
